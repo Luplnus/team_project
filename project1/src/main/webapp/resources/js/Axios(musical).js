93859a1baf9ff3e456b3721b3f8ceee1e7935bf2
clread1 <yhyungho12@gmail.com>
2025-06-02 17:48:28 +0900

    .

--- project1/src/main/webapp/resources/js/Axios(musical).js
+++ project1/src/main/webapp/resources/js/Axios(musical).js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    axios.get("https://localhost:8080/api/theaters").then(function (response) {
+    axios.get("https//localhost:8080/api/musical").then(function (response) {
         const data = response.data;
         const wrapper = document.querySelector('.main-swiper .swiper-wrapper');
 
@@ -11,14 +11,14 @@ document.addEventListener("DOMContentLoaded", function () {
             slide.className = 'swiper-slide main-slide';
 
             slide.innerHTML =
-                `<img src="${item.t_poster}" alt="${item.t_title}" onerror="this.src='https://via.placeholder.com/240x204?text=No+Image'">`
+                `<img src="${item.mu_poster}" alt="${item.mu_title}" onerror="this.src='https://via.placeholder.com/240x204?text=No+Image'">`
 
                 slide.addEventListener('click', function () {
-                    document.querySelector('.post_img').src = item.t_poster;
-                    document.querySelector('.movie_title').textContent = item.t_title;
-                    document.querySelector('.year').textContent = formatKoreanDate(item.t_start_date);
-                    document.querySelector('.runtime').textContent = formatKoreanDate(item.t_end_date);
-                    document.querySelector('.pfm_hall').textContent = item.t_venue;
+                    document.querySelector('.post_img').src = item.mu_poster;
+                    document.querySelector('.movie_title').textContent = item.mu_title;
+                    document.querySelector('.year').textContent = formatKoreanDate(item.mu_prfpdfrom);
+                    document.querySelector('.runtime').textContent = formatKoreanDate(item.mu_prfpdto);
+                    document.querySelector('.pfm_hall').textContent = item.mu_venue;
                 });
 
                 wrapper.appendChild(slide);
